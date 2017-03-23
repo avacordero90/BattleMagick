be465fe77be345531be1fe3f02bde3673710ed31
Alacr1ty <corderda@oregonstate.edu>
2017-03-23 00:35:07 -0700

    project submitted! its still very rudimentary and basic, but Im still really proud! Im gonna improve the codebase some more, and then then set up my own database and host it somewhere public!

--- battlemagick.js
+++ battlemagick.js
@@ -31,53 +31,93 @@ function showtable(table){
     } finally { console.log("Render failed.") }
 }
 
-app.get('/',function(req, res, next){
+app.get('/',function(req,res,next){
     var context = {};
-        res.render('home', context);
-    });
-
+    res.render('home',context);   
+});
+    
+//app.get('/',showtable('wizard'));
 app.get('/wizards',showtable('wizard'));
 app.get('/spells',showtable('spell'));
 app.get('/creatures',showtable('creature'));
 app.get('/potions',showtable('potion'));
 
-app.get('/insert',function(req,res,next){
+app.post('/insert',function(req,res,next){
+    if (req.body.table === 'wizard'){
+        attribs=" (`name`,`special`,`life`,`magick`)"+
+            " VALUES ((?),(?),(?),(?))";
+        fields = [req.body.name,req.body.special,req.body.life, req.body.magick];
+    } else if (req.body.table === 'potion'){
+        attribs=" (`name`) VALUES (?)";
+        fields = [req.body.name];
+    } else if (req.body.table === 'creature'){
+        attribs=" (`name`,`w_friend`,`ability`,`damage`,`life`)"+
+            " VALUES ((?),(?),(?),(?),(?))";
+        fields =[req.body.name,req.body.w_friend,req.body.ability,req.body.damage, req.body.life]
+    } else if (req.body.table === 'spell'){
+        attribs=" (`name`,`cost`,`rate`) VALUES ((?),(?),(?))";
+        fields = [req.body.name,req.body.cost,req.body.rate]
+    }
     var context = {};
-        mysql.pool.query("INSERT INTO wizard (`name`,`special`,`life`,`magick`) VALUES ((?),(?),(?),(?))", [req.query.name, req.query.special, req.query.life, req.query.magick], function(err, result){
+    console.log=(req.body)
+        mysql.pool.query("INSERT INTO "+req.body.table+attribs, fields, function(err, result){
             if(err){
                 next(err);
                 return;
             }
-        mysql.pool.query('SELECT * FROM wizard', function(err, rows, fields){
-		if(err){
-			next(err);
-			return;
-		}
-		context.results = rows;
-		res.render('table', context);
-	});
-        });
+        mysql.pool.query('SELECT * FROM '+req.body.table, function(err, rows, fields){
+        if(err){
+            next(err);
+            return;
+        }
+        context.fields = fields;
+        context.results = rows;
+        context.table = req.body.table;
+        res.render('table', context);
+       });
+    });
 });
 
-app.get('/delete',function(req,res,next){
+app.post('/delete', function(req,res,next){
   var context = {};
-  mysql.pool.query("DELETE FROM wizard WHERE `id`=(?)", [req.query.id], function(err, result){
+  mysql.pool.query("DELETE FROM "+req.body.table+" WHERE `id`=(?)", parseInt(req.body.id), function(err, result){
     if(err){
       next(err);
       return;
     }	
-    mysql.pool.query('SELECT * FROM wizard', function(err, rows, fields){
+    mysql.pool.query('SELECT * FROM '+req.body.table, function(err, rows, fields){
     if(err){
         next(err);
         return;
     }
+    context.fields = fields;
     context.results = rows;
+    context.table = req.body.table;
     res.render('table', context);
-	});
+    });
   });
 });
 
-app.get('/update',function(req,res,next){
+//app.get('/delete',function(req,res,next){
+//  var context = {};
+//  mysql.pool.query("DELETE FROM "+req.query.table+" WHERE `id`=(?)", parseInt(req.query.id), function(err, result){
+//    if(err){
+//      next(err);
+//      return;
+//    }	
+//    mysql.pool.query('SELECT * FROM wizard', function(err, rows, fields){
+//    if(err){
+//        next(err);
+//        return;
+//    }
+//    context.results = rows;
+//    res.render('table', context);
+//	});
+//  });
+//});
+
+
+app.post('/update',function(req,res,next){
   var context = {};
   mysql.pool.query("SELECT * FROM wizard WHERE `id`=(?)", [req.query.id], function(err, result){
     if(err){
@@ -99,7 +139,6 @@ app.get('/update',function(req,res,next){
     }
   });
 });
-
 
 app.use(function(req,res){
   res.status(404);
